f3b13f8007080599d6a927e5248e581ac6d0441e
mertcalkan <mert.calkan@std.yeditepe.edu.tr>
2024-05-12 03:15:50 +0400

    Update page.tsx

--- social-assistant/app/(dashboard)/(routes)/suggest-channels/page.tsx
+++ social-assistant/app/(dashboard)/(routes)/suggest-channels/page.tsx
@@ -4,7 +4,7 @@ import { Heading } from "@/components/heading";
 import { Filter } from "lucide-react";
 import { Progress } from "@/components/ui/progress";
 
-const TagsInput = ({ tags, setTags, setLoading, searchChannels, selectedOption }) => {
+const TagsInput = ({ tags, setTags, setLoading, searchChannels, selectedOption}) => {
   const [inputValue, setInputValue] = useState("");
   const [error, setError] = useState(null);
   const [searchResults, setSearchResults] = useState([]);
@@ -14,20 +14,35 @@ const TagsInput = ({ tags, setTags, setLoading, searchChannels, selectedOption }
   const [maxSubscribers, setMaxSubscribers] = useState("");
   const [minVideos, setMinVideos] = useState("");
   const [maxVideos, setMaxVideos] = useState("");
-
+  const [similarChannels, setSimilarChannels] = useState([]);
+  const [similarVideos, setSimilarVideos] = useState([]);
+ 
   // selectedOption değiştiğinde tags state'ini temizle
   useEffect(() => {
     if (selectedOption === "hashtag" || selectedOption === "channelName" || selectedOption === "channelUrl" || selectedOption === "videoUrl") {
       setTags([]);
     }
   }, [selectedOption, setTags]);
-
+ 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
     // Her input değiştiğinde hatayı temizle
     setError(null);
   };
-
+  const isYouTubeChannelUrl = (url) => {
+    // YouTube kanalı URL'si regex'i
+    const channelRegex = /^(https?:\/\/)?(www\.)?youtube\.com\/(channel|c)\/[a-zA-Z0-9_-]+/;
+    return channelRegex.test(url);
+  };
+  const getVideoIdFromUrl = (url) => {
+    const match = url.match(/[?&]v=([^&]+)/);
+    return match && match[1];
+  };
+  const isYouTubeVideoUrl = (url) => {
+    // YouTube video URL'si regex'i
+    const videoRegex = /^(https?:\/\/)?(www\.)?youtube\.com\/watch\?v=[a-zA-Z0-9_-]+(&\S*)?/;
+    return videoRegex.test(url);
+  };
   const handleInputKeyDown = (event) => {
     if (event.key === "Enter" && inputValue.trim() !== "") {
       setTags([...tags, inputValue.trim()]);
@@ -50,32 +65,103 @@ const TagsInput = ({ tags, setTags, setLoading, searchChannels, selectedOption }
   };
 
   const handleSearch = async () => {
-    if (tags.length === 0) {
-      setError("Please enter a tag.");
-      return;
-    }
-
-    setLoading(true);
-    setError(null);
-
+    const API_KEY = "AIzaSyBMepq0T0uNF6NVuWMI1skYVTs8HTTGEd0"; // API anahtarınızı buraya ekleyin
+    console.log(3)
     try {
-      const results = await searchChannels(
-        tags,
-        searchType,
-        subscriberRange,
-        minSubscribers,
-        maxSubscribers,
-        minVideos,
-        maxVideos
-      );
-      setSearchResults(results);
+      if (selectedOption === "hashtag") {
+        if (tags.length === 0) {
+          setError("Please enter a tag.");
+        }
+        else{
+          const hashtagReq = tags.join('+');
+          const response = await fetch(`https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&part=snippet&type=channel&q=${encodeURIComponent(hashtagReq)}`);
+          const data = await response.json();
+          setSearchResults(data.items.map(item => ({
+            title: item.snippet.title,
+            description: item.snippet.description,
+            thumbnail: item.snippet.thumbnails.default.url // Örnek olarak thumbnail alınabilir, sizin ihtiyacınıza göre düzenleyin
+          })));
+        }
+       
+      } else if (selectedOption === "channelUrl") {
+        // Kanal URL'sinden kanal ID'sini al
+        if (isYouTubeChannelUrl(inputValue)) {
+          const channelId = getChannelIdFromUrl(inputValue);
+          if (channelId) {
+            const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&order=viewCount&channelId=${channelId}&type=video&maxResults=10&key=${API_KEY}`);
+            const data = await response.json();
+            const popularVideos = data.items.map(item => ({
+              title: item.snippet.title,
+              description: item.snippet.description,
+              tags: item.snippet.tags,
+              categoryId: item.snippet.categoryId
+            }));
+            const similarChannelsData = await fetchSimilarChannels(popularVideos, API_KEY);
+            setSimilarChannels(similarChannelsData);
+          }
+        } else {
+          setError("Please enter a valid channel URL.");
+          return;
+        }
+      } else if (selectedOption === "videoUrl") {
+        // Video URL'si seçiliyse
+        if (!isYouTubeVideoUrl(inputValue)) {
+          setError("Please enter a valid video URL.");
+          return;
+        }
+        const videoId = getVideoIdFromUrl(inputValue);
+        const response = await fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=${API_KEY}`);
+        const data = await response.json();
+        const videoTags = data.items[0].snippet.tags;
+        const similarVideosData = await fetchSimilarVideos(videoTags, videoId, API_KEY);
+        setSimilarVideos(similarVideosData);
+      }
     } catch (error) {
-      console.error("Error:", error);
-      setError("An error occurred while fetching data. Please try again later.");
-    } finally {
-      setLoading(false);
+      console.error("Error during search:", error);
+      setError("An error occurred during search.");
     }
   };
+  
+  // Kanal URL'sinden kanal ID'sini almak için ayrı bir fonksiyon ekle
+  const getChannelIdFromUrl = (url) => {
+    const match = url.match(/(channel|user)\/([a-zA-Z0-9_-]+)/);
+    return match && match[2];
+  };
+  
+  // Benzer kanalları bulmak için ayrı bir fonksiyon ekle
+  const fetchSimilarChannels = async (videos, API_KEY) => {
+    const similarChannelsData = [];
+    for (const video of videos) {
+      for (const tag of video.tags) {
+        const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=${tag}&type=channel&key=${API_KEY}`);
+        const data = await response.json();
+        for (const item of data.items) {
+          const channelId = item.snippet.channelId;
+          if (!similarChannelsData.some(channel => channel.id === channelId)) {
+            similarChannelsData.push({ title: item.snippet.channelTitle, id: channelId });
+          }
+        }
+      }
+    }
+    return similarChannelsData;
+  };
+  
+  // Benzer videoları bulmak için ayrı bir fonksiyon ekle
+  const fetchSimilarVideos = async (tags, excludeVideoId, API_KEY) => {
+    const similarVideosData = [];
+    for (const tag of tags) {
+      const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=${tag}&type=video&key=${API_KEY}`);
+      const data = await response.json();
+      for (const item of data.items) {
+        if (item.id.videoId !== excludeVideoId) {
+          similarVideosData.push({ title: item.snippet.title, videoId: item.id.videoId });
+        }
+      }
+    }
+    return similarVideosData;
+  };
+  
+  
 
   return (
     <div className="mt-4">
@@ -86,8 +172,16 @@ const TagsInput = ({ tags, setTags, setLoading, searchChannels, selectedOption }
           onChange={handleInputChange}
           onKeyDown={(event) => {
             if (selectedOption === "hashtag") {
-              handleInputKeyDown(event);
-              handleBackspaceKeyDown(event);
+              if (event.key === "Enter" && inputValue.trim() !== "") {
+                setTags([...tags, inputValue.trim()]);
+                setInputValue("");
+                // Etiket eklendiğinde hatayı temizle
+                setError(null);
+              } else if (event.key === "Backspace" && inputValue === "" && tags.length > 0) {
+                const newTags = [...tags];
+                newTags.pop();
+                setTags(newTags);
+              }
             }
           }}
           placeholder={
@@ -143,6 +237,26 @@ const TagsInput = ({ tags, setTags, setLoading, searchChannels, selectedOption }
           ))}
         </div>
       )}
+      {similarChannels.length > 0 && (
+        <div className="mt-4">
+          <h2 className="text-lg font-semibold">Similar Channels:</h2>
+          <ul className="list-disc pl-8">
+            {similarChannels.map((channel, index) => (
+              <li key={index}>{channel.title}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+      {similarVideos.length > 0 && (
+        <div className="mt-4">
+          <h2 className="text-lg font-semibold">Similar Videos:</h2>
+          <ul className="list-disc pl-8">
+            {similarVideos.map((video, index) => (
+              <li key={index}>{video.title}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
@@ -158,12 +272,68 @@ const SuggestIdeasPage = () => {
   const [maxSubscribers, setMaxSubscribers] = useState("");
   const [minVideos, setMinVideos] = useState("");
   const [maxVideos, setMaxVideos] = useState("");
-  const [selectedOption, setSelectedOption] = useState("hashtag"); // Varsayılan seçenek: hashtag
+  let [selectedOption, setSelectedOption] = useState(""); // Varsayılan seçenek: hashtag
 
   const toggleFilters = () => {
     setShowFilters(!showFilters);
   };
-
+  const handleSearchVariant = async () => {
+    const API_KEY = "AIzaSyBMepq0T0uNF6NVuWMI1skYVTs8HTTGEd0"; // API anahtarınızı buraya ekleyin
+    console.log(3)
+    try {
+      if (selectedOption === "hashtag") {
+        if (tags.length === 0) {
+          setError("Please enter a tag.");
+        }
+        else{
+          const hashtagReq = tags.join('+');
+          const response = await fetch(`https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&part=snippet&type=channel&q=${encodeURIComponent(hashtagReq)}`);
+          const data = await response.json();
+          setSearchResults(data.items.map(item => ({
+            title: item.snippet.title,
+            description: item.snippet.description,
+            thumbnail: item.snippet.thumbnails.default.url // Örnek olarak thumbnail alınabilir, sizin ihtiyacınıza göre düzenleyin
+          })));
+        }
+       
+      } else if (selectedOption === "channelUrl") {
+        // Kanal URL'sinden kanal ID'sini al
+        if (isYouTubeChannelUrl(inputValue)) {
+          const channelId = getChannelIdFromUrl(inputValue);
+          if (channelId) {
+            const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&order=viewCount&channelId=${channelId}&type=video&maxResults=10&key=${API_KEY}`);
+            const data = await response.json();
+            const popularVideos = data.items.map(item => ({
+              title: item.snippet.title,
+              description: item.snippet.description,
+              tags: item.snippet.tags,
+              categoryId: item.snippet.categoryId
+            }));
+            const similarChannelsData = await fetchSimilarChannels(popularVideos, API_KEY);
+            setSimilarChannels(similarChannelsData);
+          }
+        } else {
+          setError("Please enter a valid channel URL.");
+          return;
+        }
+      } else if (selectedOption === "videoUrl") {
+        // Video URL'si seçiliyse
+        if (!isYouTubeVideoUrl(inputValue)) {
+          setError("Please enter a valid video URL.");
+          return;
+        }
+        const videoId = getVideoIdFromUrl(inputValue);
+        const response = await fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=${API_KEY}`);
+        const data = await response.json();
+        const videoTags = data.items[0].snippet.tags;
+        const similarVideosData = await fetchSimilarVideos(videoTags, videoId, API_KEY);
+        setSimilarVideos(similarVideosData);
+      }
+    } catch (error) {
+      console.error("Error during search:", error);
+      setError("An error occurred during search.");
+    }
+  };
   const searchChannels = async (
     tags,
     searchType,
@@ -201,6 +371,7 @@ const SuggestIdeasPage = () => {
 
   const handleOptionChange = (event) => {
     setSelectedOption(event.target.value);
+   
   };
 
   return (
@@ -241,57 +412,46 @@ const SuggestIdeasPage = () => {
           </select>
         </div>
         {selectedOption === "hashtag" ? (
-          <TagsInput
-            tags={tags}
-            setTags={setTags}
-            setLoading={setLoading}
-            searchChannels={searchChannels}
-            selectedOption={selectedOption} // Burada selectedOption değerini props olarak iletiyoruz
-          />
-        ) : (
-          <div className="mt-4">
-            <div className="flex flex-col md:flex-row items-center border border-gray-300 rounded-lg px-4 py-2">
-              <input
-                type="text"
-                placeholder={
-                  selectedOption === "hashtag"
-                    ? "Add tags..."
-                    : `Enter ${
-                        selectedOption === "channelName"
-                          ? "channel name"
-                          : selectedOption === "channelUrl"
-                          ? "channel URL"
-                          : "video URL"
-                      }...`
-                }
-                className="w-full md:w-2/3 focus:outline-none rounded-lg mb-2 md:mb-0 md:mr-2"
-              />
+ <TagsInput
+ tags={tags}
+ setTags={setTags}
+ setLoading={setLoading}
+ searchChannels={searchChannels}
+ selectedOption={selectedOption}
 
-              <button
-                className="md:ml-auto mt-2 md:mt-0 md:ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg"
-              >
-                Search
-              </button>
-            </div>
-          </div>
-        )}
+/>
+) : (
+  <div className="mt-4">
+    <div className="flex flex-col md:flex-row items-center border border-gray-300 rounded-lg px-4 py-2">
+      <input
+        type="text"
+        placeholder={
+          selectedOption === "channelName"
+            ? "Enter channel name..."
+            : selectedOption === "channelUrl"
+            ? "Enter channel URL..."
+            : "Enter video URL..."
+        }
+        className="w-full md:w-2/3 focus:outline-none rounded-lg mb-2 md:mb-0 md:mr-2"
+      />
+
+      <button onClick={handleSearchVariant}
+        
+        className="md:ml-auto mt-2 md:mt-0 md:ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg"
+      >
+        Search
+      </button>
+    </div>
+  </div>
+)}
+
       </div>
       {showFilters && (
         <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-50">
           <div className="bg-white rounded-lg p-8 w-full md:w-1/2">
             <h2 className="text-lg font-semibold mb-4">Filter Options</h2>
             <div className="flex flex-col space-y-4">
-              <label htmlFor="searchType">Search Type:</label>
-              <select
-                id="searchType"
-                value={searchType}
-                onChange={(e) => setSearchType(e.target.value)}
-                className="border border-gray-300 rounded-lg focus:outline-none px-4 py-2"
-              >
-                <option value="channel">Only Channel Name</option>
-                <option value="shorts">Shorts Video</option>
-                <option value="long">Long Video</option>
-              </select>
+              
               <label htmlFor="subscriberRange">Subscriber Range:</label>
               <select
                 id="subscriberRange"
@@ -342,34 +502,33 @@ const SuggestIdeasPage = () => {
                 <div className="flex space-x-4">
                   <input
                     type="number"
-                    placeholder="Min Video Count"
+                    placeholder="Min Videos"
                     value={minVideos}
                     onChange={(e) => setMinVideos(e.target.value)}
                     className="border border-gray-300 rounded-lg focus:outline-none px-4 py-2 w-1/2"
                   />
                   <input
                     type="number"
-                    placeholder="Max Video Count"
+                    placeholder="Max Videos"
                     value={maxVideos}
                     onChange={(e) => setMaxVideos(e.target.value)}
                     className="border border-gray-300 rounded-lg focus:outline-none px-4 py-2 w-1/2"
                   />
                 </div>
               )}
-              <button
-                onClick={handleApplyFilters}
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg"
-              >
-                Apply Filters
-              </button>
-              <button
-                onClick={toggleFilters}
-                className="absolute top-0 right-0 mt-4 mr-4 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-lg"
-              >
-                &#10006;
-              </button>
             </div>
+            <button
+              onClick={handleApplyFilters}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg mt-4"
+            >
+              Apply Filters
+            </button>
           </div>
+        </div>
+      )}
+      {loading && (
+        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center">
+          <Progress />
         </div>
       )}
     </div>
