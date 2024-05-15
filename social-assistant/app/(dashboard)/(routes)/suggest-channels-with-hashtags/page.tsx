b0b33d6d38c252d06cf3e1f87f1cffb6a61088b5
mertcalkan <mert.calkan@std.yeditepe.edu.tr>
2024-05-15 10:51:22 +0400

    filter max min ui update

--- social-assistant/app/(dashboard)/(routes)/suggest-channels-with-hashtags/page.tsx
+++ social-assistant/app/(dashboard)/(routes)/suggest-channels-with-hashtags/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { useState } from "react";
 import { Heading } from "@/components/heading";
 import { Filter, MessageCircleQuestion } from "lucide-react";
@@ -15,7 +15,11 @@ const TagsInput = ({ tags, setTags, handleSearch }) => {
     if (event.key === "Enter" && inputValue.trim() !== "") {
       setTags([...tags, inputValue.trim()]);
       setInputValue("");
-    } else if (event.key === "Backspace" && inputValue === "" && tags.length > 0) {
+    } else if (
+      event.key === "Backspace" &&
+      inputValue === "" &&
+      tags.length > 0
+    ) {
       const newTags = [...tags];
       newTags.pop();
       setTags(newTags);
@@ -68,22 +72,40 @@ const SuggestChannelsWithHashtags = () => {
   const [tags, setTags] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-  const [searchResults, setSearchResults] = useState([]);
-  const [channelCount, setChannelCount] = useState("");
-  const [minSubscribers, setMinSubscribers] = useState("");
-  const [maxSubscribers, setMaxSubscribers] = useState("");
-  const [minVideos, setMinVideos] = useState("");
-  const [maxVideos, setMaxVideos] = useState("");
-  const [showCustomSubscriberRange, setShowCustomSubscriberRange] = useState(false);
-  const [showCustomVideoCountRange, setShowCustomVideoCountRange] = useState(false);
+  let [searchResults, setSearchResults] = useState([]);
+  let [channelCount, setChannelCount] = useState("");
+  let [minSubscribers, setMinSubscribers] = useState("");
+  let[maxSubscribers, setMaxSubscribers] = useState("");
+  let [minVideos, setMinVideos] = useState("");
+  let [maxVideos, setMaxVideos] = useState("");
+  const [showCustomSubscriberRange, setShowCustomSubscriberRange] =
+    useState(false);
+  const [showCustomVideoCountRange, setShowCustomVideoCountRange] =
+    useState(false);
 
   const handleSearch = async () => {
+   
     setLoading(true);
     const API_KEY = "YOUR_API_KEY"; // Replace with your API key
     try {
       if (tags.length === 0) {
         setError("Please enter a tag.");
       } else {
+        if(maxVideos < minVideos){
+          setMaxVideos(minVideos)
+          setMinVideos(maxVideos)
+          let tempMinVideos = minVideos
+          minVideos = maxVideos
+          maxVideos = tempMinVideos; 
+          console.log(minVideos,  maxVideos)
+        }
+        if(maxSubscribers < minSubscribers){
+          setMaxSubscribers(minSubscribers)
+          setMinSubscribers(maxSubscribers)
+          let tempMinSubscribers = minSubscribers
+          minSubscribers = maxSubscribers;
+          maxSubscribers = tempMinSubscribers
+        }
         const hashtagReq = tags.join("+");
         const response = await fetch(
           `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&part=snippet&type=channel&q=${encodeURIComponent(
@@ -113,11 +135,8 @@ const SuggestChannelsWithHashtags = () => {
     console.log("Generating ideas for all results");
   };
 
-
-
   return (
     <div>
-      
       <Heading
         title="Suggest Channels With Hashtags"
         description="You can have a look YouTube channels based on your hashtag(s) criteria."
@@ -127,152 +146,175 @@ const SuggestChannelsWithHashtags = () => {
       />
       <div className="app px-8 ">
         <div className="flex items-center">
-        <div className="bg-white rounded-g p-4">
- 
-  
-
-
-
-
-
-
-
-      
+          <div className="bg-white rounded-g"></div>
         </div>
-         
-        </div>
-        <h1 className="mt-4 text-l font-semibold mr-4">Filter Options</h1>   
+        <h1 className="mt-4 text-l font-semibold mr-4">Filter Options</h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-    <div>
-      <label htmlFor="channelCount" className="block">
-        Number of Channels to Display
-      </label>
-      <input
-        type="text"
-        id="channelCount"
-        value={channelCount}
-        onChange={(e) => {
-          let value = e.target.value;
-          // Sadece rakamları kabul et
-          value = value.replace(/\D/g, "");
-          // En fazla 2 haneli olacak şekilde sınırlandır
-          value = value.slice(0, 2);
-          // Sıfır yazılamayacak
-          if (value.startsWith("0")) {
-            value = value.substring(1);
-          }
-          // - sembolünü engelle
-          if (value.includes("-")) {
-            value = value.replace("-", "");
-          }
-          // 50'den fazla kanal girişi yapılamayacak
-          if (parseInt(value) > 50) {
-            value = "50";
-          }
-          // Set the final value
-          setChannelCount(value);
-        }}
-        className="border border-gray-300 rounded-lg focus:outline-none px-4 py-2 w-full"
-      />
-    </div>
+          <div>
+            <label htmlFor="channelCount" className="block">
+              Number of Channels
+            </label>
+            <input
+              type="text"
+              id="channelCount"
+              value={channelCount}
+              onChange={(e) => {
+                let value = e.target.value;
+                // Sadece rakamları kabul et
+                value = value.replace(/\D/g, "");
+                // En fazla 2 haneli olacak şekilde sınırlandır
+                value = value.slice(0, 2);
+                // Sıfır yazılamayacak
+                if (value.startsWith("0")) {
+                  value = value.substring(1);
+                }
+                // - sembolünü engelle
+                if (value.includes("-")) {
+                  value = value.replace("-", "");
+                }
+                // 50'den fazla kanal girişi yapılamayacak
+                if (parseInt(value) > 50) {
+                  value = "50";
+                }
+                // Set the final value
+                setChannelCount(value);
+              }}
+              className="border border-gray-300 rounded-lg focus:outline-none px-4 py-2 w-full"
+            />
+          </div>
 
-    <div>
-      
-      <label htmlFor="subscriberRange" className="block">
-        Subscriber Range:
-      </label>
-      <select
-        id="subscriberRange"
-        value={showCustomSubscriberRange ? "custom" : minSubscribers + "-" + maxSubscribers}
-        onChange={(e) => {
-          const [min, max] = e.target.value.split("-");
-          if (e.target.value === "custom") {
-            setShowCustomSubscriberRange(true);
-          } else {
-            setShowCustomSubscriberRange(false);
-            setMinSubscribers(min);
-            setMaxSubscribers(max);
-          }
-        }}
-        className="border border-gray-300 rounded-lg focus:outline-none px-4 py-2 w-full"
-      >
-        <option value="0-1000">0 - 1000</option>
-        <option value="1000-10000">1000 - 10,000</option>
-        <option value="10000-100000">10,000 - 100,000</option>
-        <option value="100000-1000000">100,000 - 1,000,000</option>
-        <option value="1000000-10000000">1,000,000 - 10,000,000</option>
-        <option value="custom">Custom Range</option>
-      </select>
-      {showCustomSubscriberRange && (
-        <div className="flex space-x-4 mt-2">
-          <input
-            type="number"
-            placeholder="Min Subscribers"
-            value={minSubscribers}
-            onChange={(e) => setMinSubscribers(e.target.value)}
-            className="border border-gray-300 rounded-lg focus:outline-none px-4 py-2 w-1/2"
-          />
-          <input
-            type="number"
-            placeholder="Max Subscribers"
-            value={maxSubscribers}
-            onChange={(e) => setMaxSubscribers(e.target.value)}
-            className="border border-gray-300 rounded-lg focus:outline-none px-4 py-2 w-1/2"
-          />
-        </div>
-      )}
-    </div>
+          <div>
+            <label htmlFor="subscriberRange" className="block">
+              Subscriber Range:
+            </label>
+            <select
+              id="subscriberRange"
+              value={
+                showCustomSubscriberRange
+                  ? "custom"
+                  : minSubscribers + "-" + maxSubscribers
+              }
+              onChange={(e) => {
+                // const [min, max] = e.target.value.split("-");
+                if (e.target.value === "custom") {
+                  setShowCustomSubscriberRange(true);
+                } else {
+                  setShowCustomSubscriberRange(false);
+                  // setMinSubscribers(min);
+                  // setMaxSubscribers(max);
+                }
+              }}
+              className="border border-gray-300 rounded-lg focus:outline-none px-4 py-2 w-full"
+            >
+              <option value="0-1000">0 - 1000</option>
+              <option value="1000-10000">1000 - 10,000</option>
+              <option value="10000-100000">10,000 - 100,000</option>
+              <option value="100000-1000000">100,000 - 1,000,000</option>
+              <option value="1000000-10000000">1,000,000 - 10,000,000</option>
+              <option value="custom">Custom Range</option>
+            </select>
+            {showCustomSubscriberRange && (
+              <div className="flex space-x-4 mt-2">
+                <input
+                  type="text"
+                  placeholder="Min Subscribers"
+                  value={minSubscribers}
+                  onChange={(e) => {
+                    const value = e.target.value;
+                    // Allow only numeric characters and handle backspace
+                    if (/^\d*$/.test(value) || value === "") {
+                      setMinSubscribers(value);
+                    }
+                  }}
+                  maxLength="11"
+                  className="border border-gray-300 rounded-lg focus:outline-none px-4 py-2 w-1/2"
+                />
+                <input
+                  type="text"
+                  placeholder="Max Subscribers"
+                  value={maxSubscribers}
+                  onChange={(e) => {
+                    const value = e.target.value;
+                    // Allow only numeric characters and handle backspace
+                    if (/^\d*$/.test(value) || value === "") {
+                      setMaxSubscribers(value);
+                    }
+                  }}
+                  maxLength="11"
+                  className="border border-gray-300 rounded-lg focus:outline-none px-4 py-2 w-1/2"
+                />
+              </div>
+            )}
+          </div>
 
-    <div>
-      <label htmlFor="videoCountRange" className="block">
-        Video Count Range:
-      </label>
-      <select
-        id="videoCountRange"
-        value={showCustomVideoCountRange ? "custom" : minVideos + "-" + maxVideos}
-        onChange={(e) => {
-          const [min, max] = e.target.value.split("-");
-          if (e.target.value === "custom") {
-            setShowCustomVideoCountRange(true);
-          } else {
-            setShowCustomVideoCountRange(false);
-            setMinVideos(min);
-            setMaxVideos(max);
-          }
-        }}
-        className="border border-gray-300 rounded-lg focus:outline-none px-4 py-2 w-full"
-      >
-        <option value="0-1000">0 - 1000</option>
-        <option value="1000-10000">1000 - 10,000</option>
-        <option value="10000-100000">10,000 - 100,000</option>
-        <option value="100000-1000000">100,000 - 1,000,000</option>
-        <option value="1000000-10000000">1,000,000 - 10,000,000</option>
-        <option value="custom">Custom Range</option>
-      </select>
-      {showCustomVideoCountRange && (
-        <div className="flex space-x-4 mt-2">
-          <input
-            type="number"
-            placeholder="Min Video Count"
-            value={minVideos}
-            onChange={(e) => setMinVideos(e.target.value)}
-            className="border border-gray-300 rounded-lg focus:outline-none px-4 py-2 w-1/2"
-          />
-          <input
-            type="number"
-            placeholder="Max Video Count"
-            value={maxVideos}
-            onChange={(e) => setMaxVideos(e.target.value)}
-            className="border border-gray-300 rounded-lg focus:outline-none px-4 py-2 w-1/2"
-          />
+          <div>
+            <label htmlFor="videoCountRange" className="block">
+              Video Count Range:
+            </label>
+            <select
+              id="videoCountRange"
+              value={
+                showCustomVideoCountRange
+                  ? "custom"
+                  : minVideos + "-" + maxVideos
+              }
+              onChange={(e) => {
+                // const [min, max] = e.target.value.split("-");
+                if (e.target.value === "custom") {
+                  setShowCustomVideoCountRange(true);
+                } else {
+                  setShowCustomVideoCountRange(false);
+                  // setMinVideos(min);
+                  // setMaxVideos(max);
+                }
+              }}
+              className="border border-gray-300 rounded-lg focus:outline-none px-4 py-2 w-full"
+            >
+              <option value="0-1000">0 - 1000</option>
+              <option value="1000-10000">1000 - 10,000</option>
+              <option value="10000-100000">10,000 - 100,000</option>
+              <option value="100000-1000000">100,000 - 1,000,000</option>
+              <option value="1000000-10000000">1,000,000 - 10,000,000</option>
+              <option value="custom">Custom Range</option>
+            </select>
+            {showCustomVideoCountRange && (
+              <div className="flex space-x-4 mt-2">
+                <input
+                  type="text"
+                  placeholder="Min Video Count"
+                  value={minVideos}
+                  onChange={(e) => {
+                    const value = e.target.value;
+                    // Allow only numeric characters and handle backspace
+                    if (/^\d*$/.test(value) || value === "") {
+                      setMinVideos(value);
+                    }
+                  }}
+                  maxLength="11"
+                  className="border border-gray-300 rounded-lg focus:outline-none px-4 py-2 w-1/2"
+                />
+                <input
+                  type="text"
+                  placeholder="Max Video Count"
+                  value={maxVideos}
+                  onChange={(e) => {
+                    const value = e.target.value;
+                    // Allow only numeric characters and handle backspace
+                    if (/^\d*$/.test(value) || value === "") {
+                      setMaxVideos(value);
+                    }
+                  }}
+                  maxLength="11"
+                  className="border border-gray-300 rounded-lg focus:outline-none px-4 py-2 w-1/2"
+                />
+              </div>
+            )}
+          </div>
         </div>
-      )}
-    </div>
-  </div>
-        <h1 className="mt-4 text-xl font-semibold mr-4">Search the channel based on your taste!</h1>
+        <h1 className="mt-8 text-xl font-semibold mr-4">
+          Search the channel based on your taste!
+        </h1>
         <TagsInput tags={tags} setTags={setTags} handleSearch={handleSearch} />
-
-        
       </div>
 
       {loading && (
@@ -280,9 +322,7 @@ const SuggestChannelsWithHashtags = () => {
           <Progress />
         </div>
       )}
-      {error && (
-        <div className="mt-2 text-red-500 text-center">{error}</div>
-      )}
+      {error && <div className="mt-2 text-red-500 text-center">{error}</div>}
       <div className="grid grid-cols-3 gap-4 mt-4">
         {searchResults.map((result, index) => (
           <div key={index} className="border p-4 ml-8 mr-8 ">
